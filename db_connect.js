6700e8c507227e1940e68a59fc059c8b4fc527e1
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-04-27 12:14:47 -0400

    Put all DB related code into db_connect,

--- db_connect.js
+++ db_connect.js
@@ -3,6 +3,8 @@
 
 // require mysql module
 const mysql = require('mysql');
+// require Util so I can use promisify
+const util = require('util');
 
 // connect to database - configure to your local settings
 // sql will seed into database employee_tracker
@@ -10,7 +12,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'rXwAP9eJ',
     database: 'employee_tracker',
 });
 
@@ -18,4 +20,7 @@ connection.connect(function(err) {
     if (err) throw err;
 });
 
-module.exports = connection;
+// use promisify so that I can use async/await with query
+let query = util.promisify(connection.query).bind(connection);
+
+module.exports = { connection, query };
